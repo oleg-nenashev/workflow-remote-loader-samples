15440a3ceb479e2716334a9df13fdfa20e4f9842
Oleg Nenashev <o.v.nenashev@gmail.com>
2015-10-06 14:49:34 +0300

    upd

--- lib/helloworld.groovy
+++ lib/helloworld.groovy
@@ -5,11 +5,7 @@
 def version = '1.0'
 
 def printHello() {
-  echo "Hello, world! The version of this file is ${version}"
-}
-
-def getVersion() {
-  return version;
+  echo "Hello, world! The version of this file is ${this.version}"
 }
 
 return this;
