ac480fe18c5d9999eec6e1aea9ef3a6fb19a073f
Oleg Nenashev <o.v.nenashev@gmail.com>
2015-10-06 14:47:32 +0300

    upd

--- lib/helloworld.groovy
+++ lib/helloworld.groovy
@@ -2,10 +2,14 @@
  * Prints a stub message for testing purposes.
  */
 
-public final def version = '1.0'
+def version = '1.0'
 
 def printHello() {
   echo "Hello, world! The version of this file is ${version}"
+}
+
+def getVersion() {
+  return version;
 }
 
 return this;
