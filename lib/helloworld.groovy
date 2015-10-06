7f6b438cf33c8a6aaed0b013cc406d9c298a11cb
Oleg Nenashev <o.v.nenashev@gmail.com>
2015-10-06 14:40:53 +0300

    Add helloworld

---
+++ lib/helloworld.groovy
@@ -0,0 +1,11 @@
+/**
+ * Prints a stub message for testing purposes.
+ */
+
+def version = '1.0'
+
+def printHello() {
+  echo "Hello, world! The version of this file is ${version}"
+}
+
+return this;
